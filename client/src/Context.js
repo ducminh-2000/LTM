0c6e833950d7d917d9032270d4fae48b77ff5aa3
duc08 <duc08@DESKTOP-NVG2TNG>
2021-10-07 16:57:06 +0700

    ltm-v1

--- client/src/Context.js
+++ client/src/Context.js
@@ -4,7 +4,7 @@ import Peer from 'simple-peer';
 
 const SocketContext = createContext();
 
-const socket = io('https://serverltm.herokuapp.com/');
+const socket = io('https://serverltm.herokuapp.com');
 
 const ContextProvider = ({ children }) => {
   const [callAccepted, setCallAccepted] = useState(false);
